f8b6375a463386e7b29421719d49332741d97fe8
Christian Delgado Chavez <kevindelgadochavez.cj@gmail.com>
2020-07-11 22:53:59 -0500

    clave cifrada

--- VictoriaApp/Registro.aspx.cs
+++ VictoriaApp/Registro.aspx.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -22,9 +24,18 @@ namespace VictoriaApp
             oPersona.Apellidos = txtApellidos.Value;
             oPersona.FechaNacimiento = Convert.ToDateTime(deFechaNacimiento.Value);
             oPersona.Correo = txtCorreo.Value;
-            oPersona.Password = txtPassword.Value;
+            oPersona.Password = CifrarClave(txtPassword.Value.Trim());
             oPersona.Celular = txtCelular.Value;
 
+        }
+
+        public static string CifrarClave(string clave)
+        {
+            SHA512Managed password = new SHA512Managed();
+            byte[] texto = Encoding.ASCII.GetBytes(clave);
+            byte[] textocifrado = password.ComputeHash(texto);
+            string contrasena = Convert.ToBase64String(textocifrado);
+            return contrasena;
         }
     }
 }
